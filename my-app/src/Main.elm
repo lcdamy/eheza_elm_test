ecf124f93f7bec1e18f96392de7737bff6683dc9
lcdamy <zudanga@gmail.com>
2020-07-14 23:40:55 +0200

    after resolve the the error,thank you to elm doc

--- my-app/src/Main.elm
+++ my-app/src/Main.elm
@@ -1,93 +1,86 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, text, div, h1, img,pre)
 import Html.Attributes exposing (src)
 import RemoteData exposing (WebData)
 import Http exposing (expectJson)
 
 
----- MODEL ----
+
+-- MAIN
 
 
-type alias Model =
-    {}
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-     ( { news = Loading }
-    , getNews
-    )
 
-getNews : Cmd Msg
-getNews =
-    Http.get
-        { url = "https://hacker-news.firebaseio.com/v0/topstories.json"
-        , expect = expectJson (RemoteData.fromResult >> NewsResponse) decodeNews
-        }
+-- MODEL
 
 
----- UPDATE ----
+type Model
+  = Failure
+  | Loading
+  | Success String
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Loading
+  , Http.get
+      { url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+      , expect = Http.expectString GotText
+      }
+  )
+
+
+
+-- UPDATE
 
 
 type Msg
-    = NoOp
-    | NewsResponse (WebData News)
-
-type alias News =
-    { name : String
-    , title : String
-    , id : String
-    }
-
-type RemoteData e a
-    = NotAsked
-    | Loading
-    | Failure e
-    | Success a
+  = GotText (Result Http.Error String)
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-     case msg of
-        NewsResponse response ->
-            ( { model | news = response }
-            , Cmd.none
-            )
+  case msg of
+    GotText result ->
+      case result of
+        Ok fullText ->
+          (Success fullText, Cmd.none)
+
+        Err _ ->
+          (Failure, Cmd.none)
 
 
 
----- VIEW ----
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    case model.news of
-    NotAsked -> text "Initialising."
+  case model of
+    Failure ->
+      text "I was unable to load your book."
 
-    Loading -> text "Loading."
+    Loading ->
+      text "Loading..."
 
-    -- Failure err -> text ("Error: " ++ toString err)
-
-    Success news -> viewNews news
-
-
-viewNews : News -> Html msg
-viewNews news =
-    div []
-        [h1 [] [text "Here is the news."]
-        ]
-
-
----- PROGRAM ----
-
-
-main : Program () Model Msg
-main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
-        , update = update
-        , subscriptions = always Sub.none
-        }
+    Success fullText ->
+      pre [] [ text fullText ]
